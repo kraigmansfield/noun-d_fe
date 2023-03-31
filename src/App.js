e391de2d4f712c7a441e767c1bb1587051aa61a8
kraigmansfield <Kraig.mansfield@gmail.com>
2023-03-31 11:31:04 -0800

    added create new pin

--- src/App.js
+++ src/App.js
@@ -5,16 +5,44 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 import axios from 'axios'
 import PushPinIcon from '@mui/icons-material/PushPin'
 import StarRateIcon from '@mui/icons-material/StarRate'
+import {format} from "timeago.js"
+import Register from './Components/Register/Register'
+import Login from './Components/Login/Login'
 
 function App() {
   const [pins, setPins] = React.useState([])
+  
+  
   const [viewport, setViewport] = React.useState({
     longitude: 12.4,
     latitude: 37.8,
     zoom: 14,
   })
-
+  
   const [currentPlaceId, setCurrentPlaceId] = React.useState(null)
+  const [newPlace, setNewPlace] = React.useState(null)
+  const [title, setTitle] = React.useState(null)
+  const [description, setDescription] = React.useState(null)
+  const [currentUser, setCurrentUser] = React.useState(null)
+
+  const [showRegister, setShowRegister] = React.useState(false)
+  const [showLogin, setShowLogin] = React.useState(false)
+
+  const [rating, setRating] = React.useState(1)
+  const handleAddClick = (e) => {
+    console.log(e)
+    let lat = e.lngLat.lat
+    let lon = e.lngLat.lng
+
+    setNewPlace ({
+      lat:lat,
+      lng:lon
+    })
+  }
+
+  const handlePinSubmit = () => {
+
+  }
 
   const handleMarkerClicked = (id, lat, lon) => {
     console.log(lat)
@@ -44,6 +72,7 @@ function App() {
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_KEY}
         style={{ width: '100vw', height: '100vh' }}
         mapStyle="mapbox://styles/kraigmansfield/clfwq36do000d01mziph590uf"
+        onDblClick={handleAddClick}
       >
         <NavigationControl />
         {pins.map((p) => (
@@ -75,12 +104,74 @@ function App() {
                 </div>
 
                 <label>Information</label>
-                
+                  <div className='info'>
+                    <span className='username'>Created by <b>{p.username} </b></span>
+                    <span className='date'>{format(p.createdAt)}</span>
+                  </div>
               </Popup>
             )}
           </>
         ))}
+
+        {
+          newPlace &&
+          <Popup
+          longitude={newPlace.lng}
+          latitude={newPlace.lat}
+          closeOnClick={false}
+          closeOnMove={false}
+          onClose={() => setNewPlace(null)} 
+          anchor="left"
+          >
+            <div>
+              <form onSubmit={handlePinSubmit}>
+                <label>Title</label>
+                <input placeholder='Enter a Title'
+                onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Review</label>
+                <textarea placeholder='Say something about this place'
+                onChange={(e) => setDescription(e.target.value)}
+                />
+
+                <label>Rating</label>
+                <select onChange={(e) => setRating(e.target.value)}
+                >
+                  <option value = "1">1</option>
+                  <option value = "2">2</option>
+                  <option value = "3">3</option>
+                  <option value = "4">4</option>
+                  <option value = "5">5</option>
+
+                </select>
+                <button className='submitBtn' type="submit">Add Pin </button>
+              </form>
+            </div>
+          </Popup>
+        }
       </Map>
+
+        <div className='footer'>
+          <div className='footer_down'>
+            {
+              currentUser ? (<button className='button_logout'>Log out</button>)
+              : 
+              (
+                <div>
+                  <button className='button_login'>
+                    Login
+                  </button>
+                  <button className='button_register'>
+                    Register
+                  </button>
+                </div>
+              )
+            }
+          </div>
+        </div>
+          {showRegister && <Register />}
+          {showLogin && <Login />}
     </div>
   )
 }
