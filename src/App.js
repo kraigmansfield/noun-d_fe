eb2b97728ca58b7cfb78e97957f76866a5c287bf
kraigmansfield <Kraig.mansfield@gmail.com>
2023-04-05 17:41:41 -1000

    testing web routes

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
       if (!currentUser) {
       } else {
         // const response = await axios.post("https://noun-d-be.herokuapp.com/api/pins", newPin)
-        const response = await axios.post('https://noun-d-be.herokuapp.com/', newPin)
+        const response = await axios.post('/pins', newPin)
         console.log(response)
         setPins([...pins, response.data])
         setNewPlace(null)
